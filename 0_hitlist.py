6563f1045a0b23d6fac076ecf780d0a87bfc2a28
Alexander Weis <aweis.mee@gmail.com>
2025-03-31 01:25:42 +0200

    Updated 0_hitlist.py

--- 0_hitlist.py
+++ 0_hitlist.py
@@ -5,13 +5,16 @@ from hitlist import create
 if __name__ == "__main__":
     filename = os.path.basename(__file__)
 
-    if len(sys.argv) != 3:
+    if len(sys.argv) < 2 or len(sys.argv) > 3:
         print(f"Usage: python {filename} [icmp, tcp, udp] <port>")
         sys.exit(1)
 
     protocol = sys.argv[1]
+
     if protocol not in ["icmp", "tcp", "udp"]:
         print(f"Usage: python {filename} [icmp, tcp, udp] <port>")
-    port = sys.argv[2]
+        sys.exit(1)
+
+    port = sys.argv[2] if len(sys.argv) == 3 else ""
 
     create(protocol, port)
