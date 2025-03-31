096865e8253e92a510ff51b3c305666ff66c8952
Alexander Weis <aweis.mee@gmail.com>
2025-03-31 02:02:21 +0200

    Update

--- src/hitlist/__init__.py
+++ src/hitlist/__init__.py
@@ -2,11 +2,13 @@ import os
 import subprocess
 from datetime import datetime
 
-
 # sudo python3 create_hitlist.py icmp => targets/icmp/<timestamp>.csv
 # sudo python3 create_hitlist.py --protocol tcp --port 80 --os-detection ==> targets/tcp/80/<timestamp>.csv
 
 # --os-detection scans for OS on port 80 on application layer (e.g. http, smtp, ...) and transport layer (tcp, udp)
+
+DIR_PATH = os.path.dirname(os.path.abspath(__file__))
+
 
 def create_output_dir(protocol, port):
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -21,7 +23,7 @@ def create_output_dir(protocol, port):
 def create(protocol, port):
     output_dir = create_output_dir(protocol, port)
     if protocol == "icmp":
-        subprocess.run(["bash", "icmp/scan.sh", output_dir])
+        subprocess.run(["bash", os.path.join(DIR_PATH, "icmp/scan.sh"), output_dir])
     elif protocol == "tcp":
         pass
     elif protocol == "udp":
