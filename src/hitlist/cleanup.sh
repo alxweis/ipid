78771eaaa660520a63a6d203b883ccd99149ba06
Alexander Weis <aweis.mee@gmail.com>
2025-04-08 15:53:08 +0200

    Update

--- src/hitlist/cleanup.sh
+++ src/hitlist/cleanup.sh
@@ -14,19 +14,18 @@ count_lines() {
 cleanup() {
     # Count initial IPs
     initial_count=$(count_lines "$OUTPUT_FILE")
-    echo "Initial number of IPs: $initial_count"
+    echo "Initial number of IP addresses: $initial_count"
 
     # Deduplicate
     start_dedup=$(date +%s)
     TEMP_FILE=$(mktemp "${OUTPUT_FILE}.dedup.XXXXXX" -p .)
-    sort -u -T . "$OUTPUT_FILE" | LC_ALL=C > "$TEMP_FILE"
+    LC_ALL=C sort -u -T . "$OUTPUT_FILE" > "$TEMP_FILE"
     mv "$TEMP_FILE" "$OUTPUT_FILE"
     end_dedup=$(date +%s)
     dedup_time=$((end_dedup - start_dedup))
-
     deduped_count=$(count_lines "$OUTPUT_FILE")
     deduped_diff=$((initial_count - deduped_count))
-    echo "Number of IPs removed by deduplication: $deduped_diff"
+    echo "Deduplication: removed_count=$deduped_diff runtime=$dedup_time seconds"
 
     # Shuffle
     start_shuffle=$(date +%s)
@@ -35,15 +34,12 @@ cleanup() {
     mv "$TEMP_FILE" "$OUTPUT_FILE"
     end_shuffle=$(date +%s)
     shuffle_time=$((end_shuffle - start_shuffle))
-
-    final_count=$(count_lines "$OUTPUT_FILE")
-    echo "Final number of IPs: $final_count"
+    echo "Shuffle: runtime=$shuffle_time seconds"
 
     # Add header
     sed -i '1i IP' "$OUTPUT_FILE"
 
-    echo "Deduplication took: $dedup_time seconds"
-    echo "Shuffling took: $shuffle_time seconds"
-
+    final_count=$(count_lines "$OUTPUT_FILE")
+    echo "Final number of IP addresses: $final_count"
     echo "Results saved in $OUTPUT_FILE"
 }
