f380dd282189446bc5be250c1852ad235898996d
Alexander Weis <aweis.mee@gmail.com>
2025-05-04 16:44:20 +0200

    Update

--- src/probing/common/common.go
+++ src/probing/common/common.go
@@ -399,7 +399,7 @@ restartProbing:
 		removeProbe(target)
 	}
 	updateStats(isProbeValid, probeSentBytes)
-	log.Printf("Finished probing target=%s received=%d/%d sent_bytes=%d retry=%d", target, recvCounter, config.SEQReqCount, probeSentBytes, totalRetries-retries)
+	//log.Printf("Finished probing target=%s received=%d/%d sent_bytes=%d retry=%d", target, recvCounter, config.SEQReqCount, probeSentBytes, totalRetries-retries)
 }
 
 // Send
