845e9b85e8984dadfceda18fc003781b534e422e
Alexander Weis <aweis.mee@gmail.com>
2025-05-05 14:45:57 +0200

    Update

--- 1_probing.py
+++ 1_probing.py
@@ -3,19 +3,25 @@ import sys
 import subprocess
 from probing import PROBING_FILE, MODES
 
+filename = os.path.basename(__file__)
 
-if __name__ == "__main__":
-    filename = os.path.basename(__file__)
 
+def print_usage():
+    print("Usage:")
+    print("\n".join([f"python {filename} {mode}" for mode in MODES]))
+    sys.exit(1)
+
+
+def main():
     if len(sys.argv) != 2:
-        print("Usage:")
-        print("\n".join([f"python {filename} {mode}" for mode in MODES]))
-        sys.exit(1)
+        print_usage()
 
     mode = sys.argv[1]
     if mode in MODES:
         subprocess.run(["go", "run", PROBING_FILE, mode], check=True)
     else:
-        print("Usage:")
-        print("\n".join([f"python {filename} {mode}" for mode in MODES]))
-        sys.exit(1)
+        print_usage()
+
+
+if __name__ == "__main__":
+    main()
