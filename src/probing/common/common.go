0c956a744c61a2d16a82168b875ae5a09c0781c4
Alexander Weis <aweis.mee@gmail.com>
2025-05-07 14:24:30 +0200

    Update

--- src/probing/common/common.go
+++ src/probing/common/common.go
@@ -297,7 +297,7 @@ func Main(mode string) {
 		select {
 		case targetChan <- fields[0]: // Send target to channel TODO: Get index of "IP" column
 		case <-stopRunning:
-			log.Println("Stop filling target channel")
+			log.Printf("Stop filling target channel (current_len=%d)", len(targetChan))
 			return
 		}
 	}
