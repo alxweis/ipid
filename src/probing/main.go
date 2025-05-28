ae1fc39b76e0fceeb16f41c77c6b7bebd473e3a9
Alexander Weis <aweis.mee@gmail.com>
2025-05-28 21:34:10 +0200

    Update

--- src/probing/main.go
+++ src/probing/main.go
@@ -7,12 +7,8 @@ import (
 )
 
 func main() {
-	//test.Main("../../targets/tcp/80/2025-04-20_19-05-04/targets.csv")
-	if len(os.Args) != 2 {
-		fmt.Println("Error: Mode not specified")
-		return
-	} else if len(os.Args) != 3 {
-		fmt.Println("Error: targetsType not specified")
+	if len(os.Args) != 3 {
+		fmt.Println("Error: Mode or TargetsType not specified")
 		return
 	}
 	mode := os.Args[1]
