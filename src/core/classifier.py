b6c5bd62d49407d5af0ed6ba83a12afeb33b1fe3
Alexander Weis <aweis.mee@gmail.com>
2025-06-19 13:48:04 +0200

    Update

--- src/core/classifier.py
+++ src/core/classifier.py
@@ -1,5 +1,6 @@
 import math
 import random
+from collections import defaultdict
 from enum import Enum
 
 import numpy as np
@@ -51,8 +52,32 @@ def nrm_entropy(values: np.ndarray) -> float:
     return entropy / max_entropy
 
 
+def get_clusters(values: np.ndarray):
+    sorted_values = sorted(values)
+
+    clusters = []
+    current_cluster = [sorted_values[0]]
+
+    for i in range(1, len(sorted_values)):
+        # Check the difference to the previous number
+        diff = sorted_values[i] - sorted_values[i-1]
+
+        if diff < 100:
+            # Numbers belong to the same cluster
+            current_cluster.append(sorted_values[i])
+        else:
+            # New cluster starts
+            clusters.append(current_cluster)
+            current_cluster = [sorted_values[i]]
+
+    # Add the last cluster
+    clusters.append(current_cluster)
+
+    return clusters
+
+
 def p_value(values: np.ndarray) -> float:
-    intervals = len(values)
+    intervals = len(values) // 2
     interval_edges = np.linspace(0, MAX_IP_ID + 1, intervals + 1)
     observed_frequencies, _ = np.histogram(values, bins=interval_edges)
     expected_frequencies = np.full(intervals, len(values) / intervals)
@@ -89,14 +114,14 @@ def is_global(seq: IPIDSequence) -> bool:
     return seq.full.is_increasing()
 
 
+def is_multi_global(seq: IPIDSequence) -> bool:
+    clusters = get_clusters(seq.full.sequence)
+    return np.all(seq.full.increments >= 1) and len(clusters) <= len(seq.full.sequence) - 2
+
+
 def is_random(seq: IPIDSequence) -> bool:
     alpha = 0.01
-    return (is_uniform(seq.full.sequence, alpha) and
-            is_uniform(seq.even.sequence, alpha) and
-            is_uniform(seq.odd.sequence, alpha) and
-            is_uniform(seq.full.increments, alpha) and
-            is_uniform(seq.even.increments, alpha) and
-            is_uniform(seq.odd.increments, alpha))
+    return np.all(seq.full.increments >= 1) and is_uniform(seq.full.increments, alpha)
 
 
 def is_anomalous(seq: IPIDSequence) -> bool:
@@ -109,6 +134,7 @@ def has_pattern(seq: IPIDSequence) -> bool:
             or is_local_eq1(seq)
             or is_local_ge1(seq)
             or is_global(seq)
+            or is_multi_global(seq)
             or is_random(seq)
     )
 
@@ -123,6 +149,8 @@ def get_pattern(seq: IPIDSequence, get_all=False) -> list[Pattern] | Pattern:
         result.append(Pattern.LOCAL_EQ1)
     if is_local_ge1(seq):
         result.append(Pattern.LOCAL_GE1)
+    if is_multi_global(seq):
+        result.append(Pattern.MULTI_GLOBAL)
     if is_random(seq):
         result.append(Pattern.RANDOM)
     if is_anomalous(seq):
@@ -183,15 +211,35 @@ def local_ge1_ip_id_sequence(length: int) -> IPIDSequence:
     return IPIDSequence(seq)
 
 
-def global_ip_id_sequence(length: int) -> IPIDSequence:
+def global_ip_id_sequence(length: int, max_inc: int = MAX_INC) -> IPIDSequence:
     seq = []
     s = random_ip_id()
     avg_inc = random.randint(1, MAX_INC)  # correlated with avg pps of device
     dev = max(int(0.5 * avg_inc), 1)  # correlated with deviation of pps of device
 
     for i in range(length):
-        s = increment_ip_id(s, clamp(avg_inc + random.randint(-dev, dev), 1, MAX_INC))
+        s = increment_ip_id(s, clamp(avg_inc + random.randint(-dev, dev), 1, max_inc))
         seq.append(s)
+    return IPIDSequence(seq)
+
+
+def multi_global_ip_id_sequence(length: int) -> IPIDSequence:
+    seq = []
+    cluster_count = random.randint(2, 4)
+
+    sizes = [length // cluster_count] * cluster_count
+    for i in range(length % cluster_count):
+        sizes[i] += 1
+
+    cluster_seqs = {
+        i: global_ip_id_sequence(length=sizes[i], max_inc=10).full.sequence.astype(int).tolist()
+        for i in range(cluster_count)
+    }
+
+    while any(cluster_seqs.values()):
+        i = random.choice([k for k in cluster_seqs if cluster_seqs[k]])
+        seq.append(cluster_seqs[i].pop(0))
+
     return IPIDSequence(seq)
 
 
@@ -207,7 +255,34 @@ pattern_generation_map = {
     Pattern.GLOBAL: global_ip_id_sequence,
     Pattern.LOCAL_EQ1: local_eq1_ip_id_sequence,
     Pattern.LOCAL_GE1: local_ge1_ip_id_sequence,
+    Pattern.MULTI_GLOBAL: multi_global_ip_id_sequence,
     Pattern.RANDOM: random_ip_id_sequence
 }
+
+# endregion
+
+
+# region Classifier Testing
+sequence_length = 20
+sequence_count_per_pattern = 10000
+for real_pattern, generator in pattern_generation_map.items():
+
+    true_classified = 0
+    false_classified_data = defaultdict(int)
+
+    for i in range(sequence_count_per_pattern):
+        seq: IPIDSequence = generator(sequence_length)
+        classified_pattern: Pattern = get_pattern(seq)
+
+        if real_pattern.value == classified_pattern.value:
+            true_classified += 1
+        else:
+            false_classified_data[classified_pattern.value] += 1
+
+    false_classified = sequence_count_per_pattern - true_classified
+    print(
+        f"{real_pattern.value}: total={sequence_count_per_pattern} true_classified={true_classified} false_classified={false_classified} recall={true_classified / (true_classified + false_classified)}"
+    )
+    print(f"False classified breakdown: {dict(false_classified_data)}")
 
 # endregion
