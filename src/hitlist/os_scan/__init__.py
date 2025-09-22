dafca9964d8789c42846dd3e5b556d4f7554011c
Alexander Weis <github@aweis.de>
2025-09-22 17:55:14 +0200

    Update

--- src/hitlist/os_scan/__init__.py
+++ src/hitlist/os_scan/__init__.py
@@ -358,16 +358,16 @@ def run_dns_scan(ips_tmp_file: str) -> str:
 
 
 def run_os_scan(ips_tmp_file: str, targets_os_file: str):
-    # snmp_ips_file, ssh_ips_file, http_ips_file, dns_ips_file = run_port_scan(ips_tmp_file)
-    #
-    # go_file = os.path.join(os.path.dirname(__file__), "main.go")
-    # executable = os.path.join(os.path.dirname(__file__), "scanner")
-    # subprocess.run(["go", "build", "-o", executable, go_file], check=True, cwd=os.path.dirname(__file__))
+    snmp_ips_file, ssh_ips_file, http_ips_file, dns_ips_file = run_port_scan(ips_tmp_file)
 
-    snmp_result_file = "targets/icmp/2025-09-22_14-26-29/snmp_os_info.csv.zst"  # run_scanner(executable, "snmp", snmp_ips_file)
-    ssh_result_file = "targets/icmp/2025-09-22_14-26-29/ssh_os_info.csv.zst"  # run_scanner(executable, "ssh", ssh_ips_file)
-    http_result_file = "targets/icmp/2025-09-22_14-26-29/http_os_info.csv.zst"  # run_http_scan(http_ips_file)
-    dns_result_file = "targets/icmp/2025-09-22_14-26-29/dns_os_info.csv.zst"  # run_dns_scan(dns_ips_file)
+    go_file = os.path.join(os.path.dirname(__file__), "main.go")
+    executable = os.path.join(os.path.dirname(__file__), "scanner")
+    subprocess.run(["go", "build", "-o", executable, go_file], check=True, cwd=os.path.dirname(__file__))
+
+    snmp_result_file = run_scanner(executable, "snmp", snmp_ips_file)
+    ssh_result_file = run_scanner(executable, "ssh", ssh_ips_file)
+    http_result_file = run_http_scan(http_ips_file)
+    dns_result_file = run_dns_scan(dns_ips_file)
 
     con = duckdb.connect()
     query = f"""
