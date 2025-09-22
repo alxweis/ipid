9d290dc203a8572cfc918267571425352e531baf
Alexander Weis <github@aweis.de>
2025-09-22 16:12:34 +0200

    Update

--- src/hitlist/os_scan/__init__.py
+++ src/hitlist/os_scan/__init__.py
@@ -44,7 +44,7 @@ def extract_os_name(expression: str) -> str | None:
     return None
 
 
-def run_port_scan(ips_tmp_file: str) -> (str, str, str, str):
+def run_port_scan(ips_tmp_file: str) -> (str, str, str):
     base_dir = os.path.dirname(ips_tmp_file)
     output_file = os.path.join(base_dir, "output.xml")
 
@@ -53,7 +53,7 @@ def run_port_scan(ips_tmp_file: str) -> (str, str, str, str):
     process = subprocess.Popen([
         "masscan",
         "-iL", ips_tmp_file,
-        "-p22,161,80,53",
+        "-p22,161,80",
         "--rate", "30000",
         "-oX", output_file
     ], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True, bufsize=1)
@@ -72,7 +72,6 @@ def run_port_scan(ips_tmp_file: str) -> (str, str, str, str):
         "22": "ssh",
         "161": "snmp",
         "80": "http",
-        "53": "dns"
     }
 
     # Initialize IP sets for each service
@@ -107,7 +106,6 @@ def run_port_scan(ips_tmp_file: str) -> (str, str, str, str):
         service_files.get("snmp"),
         service_files.get("ssh"),
         service_files.get("http"),
-        service_files.get("dns")
     )
 
 
@@ -343,7 +341,7 @@ def run_dns_scan(ips_tmp_file: str) -> str:
 
 
 def run_os_scan(ips_tmp_file: str, targets_os_file: str):
-    # snmp_ips_file, ssh_ips_file, http_ips_file, dns_ips_file = run_port_scan(ips_tmp_file)
+    # snmp_ips_file, ssh_ips_file, http_ips_file = run_port_scan(ips_tmp_file)
     #
     # go_file = os.path.join(os.path.dirname(__file__), "main.go")
     # executable = os.path.join(os.path.dirname(__file__), "scanner")
@@ -353,20 +351,18 @@ def run_os_scan(ips_tmp_file: str, targets_os_file: str):
     snmp_result_file = "targets/udp/53/2025-05-27_11-28-43/snmp_os_info.csv.zst"  # run_scanner(executable, "snmp", snmp_ips_file)
     ssh_result_file = "targets/udp/53/2025-05-27_11-28-43/ssh_os_info.csv.zst"  # un_scanner(executable, "ssh", ssh_ips_file)
     http_result_file = "targets/udp/53/2025-05-27_11-28-43/http_os_info.csv.zst"  # run_http_scan(http_ips_file)
-    dns_result_file = "targets/udp/53/2025-05-27_11-28-43/dns_os_info.csv.zst"  # run_dns_scan(dns_ips_file)
 
     con = duckdb.connect()
     query = f"""
     COPY (
         SELECT
-            COALESCE(snmp.IP, ssh.IP, http.IP, dns.IP) AS IP,
+            COALESCE(snmp.IP, ssh.IP, http.IP) AS IP,
     
             COALESCE(
               REGEXP_EXTRACT(
                 COALESCE(NULLIF(snmp.SNMP_OS_INFO,''), 
                          NULLIF(ssh.SSH_OS_INFO,''), 
                          NULLIF(http.HTTP_OS_INFO,''), 
-                         NULLIF(dns.DNS_OS_INFO,'')),
                 '(?i)({os_pattern_str})'
               ),
               ''
@@ -375,12 +371,10 @@ def run_os_scan(ips_tmp_file: str, targets_os_file: str):
             COALESCE(snmp.SNMP_OS_INFO, '')   AS SNMP_OS_INFO,
             COALESCE(ssh.SSH_OS_INFO, '')     AS SSH_OS_INFO,
             COALESCE(http.HTTP_OS_INFO, '')   AS HTTP_OS_INFO,
-            COALESCE(dns.DNS_OS_INFO, '')     AS DNS_OS_INFO
     
-        FROM read_csv('{snmp_result_file}', delim=',', header=true, compression='zstd', quote='"') snmp
-        FULL OUTER JOIN read_csv('{ssh_result_file}',  delim=',', header=true, compression='zstd', quote='"') ssh USING (IP)
-        FULL OUTER JOIN read_csv('{http_result_file}', delim=',', header=true, compression='zstd', quote='"') http USING (IP)
-        FULL OUTER JOIN read_csv('{dns_result_file}',  delim=',', header=true, compression='zstd', quote='', escape='') dns USING (IP)
+        FROM read_csv_auto('{snmp_result_file}') snmp
+        FULL OUTER JOIN read_csv_auto('{ssh_result_file}') ssh USING (IP)
+        FULL OUTER JOIN read_csv_auto('{http_result_file}') http USING (IP)
     ) TO '{targets_os_file}' (FORMAT CSV, COMPRESSION ZSTD, HEADER);
     """
     con.execute(query)
@@ -389,12 +383,10 @@ def run_os_scan(ips_tmp_file: str, targets_os_file: str):
     # os.remove(snmp_ips_file)
     # os.remove(ssh_ips_file)
     # os.remove(http_ips_file)
-    # os.remove(dns_ips_file)
     #
     # os.remove(snmp_result_file)
     # os.remove(ssh_result_file)
     # os.remove(http_result_file)
-    # os.remove(dns_result_file)
 
 
 def start(targets_path: str):
